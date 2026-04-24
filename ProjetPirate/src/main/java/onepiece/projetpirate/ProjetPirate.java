ca830df22c25c50b1dfa5e20839a40a455ec886b
Oscat725 <oscarpuente2705@gmail.com>
2026-04-24 16:45:35 +0200

    Creation du fichier du project

---
+++ ProjetPirate/src/main/java/onepiece/projetpirate/ProjetPirate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package onepiece.projetpirate;
+
+/**
+ *
+ * @author pns5022a
+ */
+public class ProjetPirate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
