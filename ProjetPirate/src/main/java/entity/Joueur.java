e73cc8cc4e0dacae0543c58d2df212772077a9cb
Eminblk <eminbelkheirpro@gmail.com>
2026-05-11 10:46:23 +0200

    merge yoakin

--- ProjetPirate/src/main/java/entity/Joueur.java
+++ ProjetPirate/src/main/java/entity/Joueur.java
@@ -6,24 +6,67 @@ public class Joueur {
 
 	private final String nom;
 	private int pointDeVie;
-	private final Pion pion;
+	private Pion pion;
+	private boolean affecteCoco;
+    private final De deUn;
+    private final De deDeux;
+	
 
 	public Joueur(String nom, Pion pion) {
 		this.nom = nom;
 		this.pointDeVie = PV_MAX;
 		this.pion = pion;
-	}
+		this.deUn = new De(); 
+        this.deDeux = new De(); 
+    }
 
-	public Integer getPointDeVie() {
+	public int getPointDeVie() {
 		return pointDeVie;
+	}
+    
+    public void setPointDeVie(int pointDeVie) {
+		this.pointDeVie = pointDeVie;
 	}
 
 	public String getNom() {
 		return nom;
 	}
+	
+	public int getPosition() {
+		return pion.getPosition();
+	}
+	
+	public void setAffecteCoco() {
+		affecteCoco = true;
+	}
+	
     
-    public Pion getPion() {
-        return pion;
+    public int deplacer(int nbDe) {
+    	int pos = pion.getPosition();
+    	pos = pos + nbDe;
+    	if (pos > 29) {
+    		pos -= (pos%30);
+    	}
+    	pion.setPosition(pos);
+    	return pos;
+    }
+    
+    public int[] lancerDe() {
+    	int[] tabValDe =new int[] {0, 0};
+    	tabValDe[0] = deUn.getValeur();
+
+    	
+    	if (affecteCoco) {
+    		affecteCoco = false;
+    		return tabValDe;
+    	}
+    	
+    	tabValDe[1] = deDeux.getValeur();
+    	return tabValDe;
+    }
+    
+    public int getValeurCoco() {
+    	return deUn.getValeur();
     }
 
 }
