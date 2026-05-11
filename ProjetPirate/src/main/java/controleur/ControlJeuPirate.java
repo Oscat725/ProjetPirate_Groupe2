55a7d0072602c5b62b80f23d847edf57cee88afd
Eminblk <eminbelkheirpro@gmail.com>
2026-05-11 11:03:03 +0200

    Version 1

--- ProjetPirate/src/main/java/controleur/ControlJeuPirate.java
+++ ProjetPirate/src/main/java/controleur/ControlJeuPirate.java
@@ -2,19 +2,19 @@ package controleur;
 
 import entity.Joueur;
 
+
 public class ControlJeuPirate {
 	private Joueur joueur;
-	//private ControDeplacer controlDeplacer;
-	//private ControlVerifierFinPartie controlVerifierFinPartie;
-	//private ControlActiverCase controlActiverCase;
-/*	
-	public ControlJeuPirate(Joueur joueur, ControDeplacer controlDeplacer,
+	private ControlDeplacer controlDeplacer;
+	private ControlVerifierFinPartie controlVerifierFinPartie;
+//	private ControlActiverCase controlActiverCase;
+
+	public ControlJeuPirate(Joueur joueur, ControlDeplacer controlDeplacer,
 			ControlVerifierFinPartie controlVerifierFinPartie, ControlActiverCase controlActiverCase) {
 		this.joueur = joueur;
 		this.controlDeplacer = controlDeplacer;
 		this.controlVerifierFinPartie = controlVerifierFinPartie;
-		this.controlActiverCase = controlActiverCase;
+//		this.controlActiverCase = controlActiverCase;
 	
 	}
-	*/
 }
