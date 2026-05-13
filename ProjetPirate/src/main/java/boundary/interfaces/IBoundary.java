60eeb7bd54ba35a1dcd2083b75c724e3a19d00fe
tiana-melanie <r.tiamela@gmail.com>
2026-05-13 09:01:03 +0200

    ajout méthode afficherPointDeVie et afficherFinDePartie dans IBoundary

--- ProjetPirate/src/main/java/boundary/interfaces/IBoundary.java
+++ ProjetPirate/src/main/java/boundary/interfaces/IBoundary.java
@@ -4,6 +4,7 @@ public interface IBoundary {
 	
 	void affichageResultatDe(int nbrDe, int valeurDe, ILancerDe ilancerDe);
 	void deplacerPirates(String nomPirate, int Cases, IDeplacerPirate iDeplacerPirate);
-	
+	void afficherPointDeVie(String nomPirate, int pv, IPointDeVie iPointDeVie);
+	void afficherFinDePartie(String nomGagnant, IFinDePartie iFinDePartie);
 	
 }
