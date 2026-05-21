f2c821844332293a25f023404fd427f401b650fa
Nolawi23 <Nolex2005@gmail.com>
2026-05-21 20:35:20 +0200

    commentaire enlever dans IBoundary

--- ProjetPirate/src/main/java/boundary/interfaces/IBoundary.java
+++ ProjetPirate/src/main/java/boundary/interfaces/IBoundary.java
@@ -1,16 +1,13 @@
 package boundary.interfaces;
 
 public interface IBoundary {
-	//Doit prendre deux valeur de deux separemment pour qu'on affiche les valeurs de dé séparemment
-	void affichageResultatDe(int valeurDe1, int valeurDe2, ILancerDe callback);
-	void deplacerPirates(String nomPirate, int ancienneCase, int nouvelleCase, IDeplacerPirate callback);
-	void afficherEffetCase(String typeCase, String message, IActiverCase callback);
-	void afficherPointDeVie(String nomPirate, int pv, IPointsDeVie callback);
-	void afficherFinDePartie(String nomGagnant, IFinDePartie callback);
-	
-	 // Affichages simples sans callback d'interface (pas besoin d'attendre)
-    void afficherMessage(String message);                // pour les cases spéciales
-    void changerJoueurActif(String nomPirate, IControlJeuPirate callback);		// indiquer à qui est le tour
+    void affichageResultatDe(int valeurDe1, int valeurDe2, ILancerDe callback);
+    void deplacerPirates(String nomPirate, int ancienneCase, int nouvelleCase, IDeplacerPirate callback);
+    void afficherEffetCase(String typeCase, String message, IActiverCase callback);
+    void afficherPointDeVie(String nomPirate, int pv, IPointsDeVie callback);
+    void afficherFinDePartie(String nomGagnant, IFinDePartie callback);
+    void afficherMessage(String message);                
+    void changerJoueurActif(String nomPirate, IControlJeuPirate callback);		
     void commencerPartie(ICommencerPartie callback); 
     void demanderUtilisationCoco(IControlCacherDe callback);
     
--- ProjetPirate/src/main/java/controleur/ControlCacherDe.java
+++ ProjetPirate/src/main/java/controleur/ControlCacherDe.java
@@ -5,35 +5,38 @@ import boundary.interfaces.IControlCacherDe;
 import entity.Joueur;
 import entity.Jeu;
 
-public class ControlCacherDe implements IControlCacherDe{
+public class ControlCacherDe implements IControlCacherDe {
 	private Joueur[] joueurs;
 	private IBoundary iBoundary;
 	private Jeu jeu;
 	private ControlJeuPirate controlJeuPirate;
-	
+
 	public ControlCacherDe(Joueur[] joueur, IBoundary iBoundary, Jeu jeu, ControlJeuPirate controlJeuPirate) {
 		this.joueurs = joueur;
 		this.iBoundary = iBoundary;
 		this.jeu = jeu;
 		this.controlJeuPirate = controlJeuPirate;
 	}
-	
+
 	public void setAffecteCoco(Joueur joueurAffecte, boolean estAffecte) {
 		joueurAffecte.setAffecteCoco(estAffecte);
 	}
+
 	@Override
-	public void demanderUtilisationCoco(){
+	public void demanderUtilisationCoco() {
 		System.out.println("YESS");
 		if (jeu.getJoueurCourant().hasCoco()) {
-                        System.out.println("entered");
+			System.out.println("entered");
 			iBoundary.demanderUtilisationCoco(this);
+		} else {
+			controlJeuPirate.apresDemandeCoco();
 		}
-                controlJeuPirate.apresDemandeCoco();
 	}
+
 	@Override
-	public void finDemandeCoco(String reponse){
+	public void finDemandeCoco(String reponse) {
 		if (reponse.equalsIgnoreCase("oui")) {
-			joueurs[(jeu.getIndiceJoueurCourant()+1) % 2].setAffecteCoco(true);
+			joueurs[(jeu.getIndiceJoueurCourant() + 1) % 2].setAffecteCoco(true);
 			joueurs[jeu.getIndiceJoueurCourant()].setHasCoco(false);
 		}
 		controlJeuPirate.apresDemandeCoco();
