5ce3f1b22b50584aff2f25c28f0ae29c736a0735
Lauriana <lauriana.be-manantsoa@univ-tlse3.fr>
2026-05-25 21:48:43 +0200

    création de la classe pour le JPanelCoco

---
+++ ProjetPirate/src/main/java/presentation/PanelCoco.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package presentation;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author beman
+ */
+public class PanelCoco extends JPanel{
+    
+}
